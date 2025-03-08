0d92004fa942058fc219b52ba3ae77741bbc63db
michealroberts <michael@observerly.com>
2025-03-08 10:10:09 +0000

    feat: add refid REFERENCE_IDS structure in ntps module
    feat: add refid REFERENCE_IDS structure in ntps module

--- src/ntps/refid.py
+++ src/ntps/refid.py
@@ -5,7 +5,7 @@
 
 # **************************************************************************************
 
-from typing import Literal
+from typing import Dict, Final, Literal
 
 # **************************************************************************************
 
@@ -32,5 +32,31 @@ ReferenceID = Literal[
     "PTB",
     "DFM",
 ]
+
+# **************************************************************************************
+
+# Define a dictionary mapping each reference ID to its descriptive comment:
+REFERENCE_IDS: Final[Dict[ReferenceID, str]] = {
+    "GOES": "Geosynchronous Orbit Environment Satellite",
+    "GPS": "Global Positioning System",
+    "GAL": "Galileo Positioning System",
+    "PPS": "Generic pulse-per-second",
+    "IRIG": "Inter-Range Instrumentation Group",
+    "WWVB": "LF Radio WWVB Ft. Collins, CO 60 kHz",
+    "DCF": "LF Radio DCF77 Mainflingen, DE 77.5 kHz",
+    "HBG": "LF Radio HBG Prangins, HB 75 kHz",
+    "MSF": "LF Radio MSF Anthorn, UK 60 kHz",
+    "JJY": "LF Radio JJY Fukushima, JP 40 kHz, Saga, JP 60 kHz",
+    "LORC": "MF Radio LORAN C station, 100 kHz",
+    "TDF": "MF Radio Allouis, FR 162 kHz",
+    "CHU": "HF Radio CHU Ottawa, Ontario",
+    "WWV": "HF Radio WWV Ft. Collins, CO",
+    "WWVH": "HF Radio WWVH Kauai, HI",
+    "NIST": "NIST telephone modem",
+    "ACTS": "NIST telephone modem",
+    "USNO": "USNO telephone modem",
+    "PTB": "European telephone modem",
+    "DFM": "UTC(DFM)",
+}
 
 # **************************************************************************************
