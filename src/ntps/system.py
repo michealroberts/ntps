d82bb349a3084ea376949991075cc8fe77577d23
michealroberts <michael@observerly.com>
2025-05-28 13:06:47 +0100

    feat: add set_system_time to system in ntps module
    feat: add set_system_time to system in ntps module

--- src/ntps/system.py
+++ src/ntps/system.py
@@ -5,7 +5,8 @@
 
 # **************************************************************************************
 
-from time import time
+import errno
+from time import CLOCK_REALTIME, clock_settime, time
 
 # **************************************************************************************
 
@@ -21,6 +22,27 @@ def get_ntp_time() -> float:
     Assumes the system time is GPS-synced externally.
     """
     return time() + NTP_TIMESTAMP_DELTA
+
+
+# **************************************************************************************
+
+
+def set_system_time(when: float) -> None:
+    """
+    Set the system clock (CLOCK_REALTIME) to timestamp (seconds since epoch,
+    as a float.
+
+    Raises PermissionError if you're not privileged, or OSError on other failure.
+    """
+    try:
+        clock_settime(CLOCK_REALTIME, when)
+    except OSError as e:
+        # Map EPERM → PermissionError for clarity:
+        if e.errno == errno.EPERM:
+            raise PermissionError(
+                "setting system time requires root/administrator"
+            ) from e
+        raise
 
 
 # **************************************************************************************
